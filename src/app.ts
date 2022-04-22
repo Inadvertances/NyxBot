951d4d3842fd85aa9d41a4fc848a419136e141c5
Inadvertances <yueliang.chester@gmail.com>
2022-04-22 14:05:57 +0200

    server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import * as dotenv from "dotenv";
+import { Server } from "http";
 import { DiscordBot } from "./DiscordBot/DiscordBot";
 
 dotenv.config();
 if (process.env.CLIENT_TOKEN) {
     DiscordBot.setupBot(process.env.CLIENT_TOKEN);
 }
+
+let server: Server = new Server();
+
+server.listen({ port: process.env.PORT || 5000})
